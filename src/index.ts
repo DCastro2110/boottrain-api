a10344f4b5eccabd441d3584bc3259fd986a29bc
Davi Castro <santos.davi2110@gmail.com>
2026-04-15 15:18:35 -0300

    chore: add zod fastify integration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import console from "console";
 import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
 
 const app = fastify({
   logger: true,
 });
 
-app.get("/", async (request, reply) => {
-  return { message: "Hello, World!" };
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: async (request, reply) => {
+    return { message: "Hello, World!" };
+  },
 });
 
 try {
