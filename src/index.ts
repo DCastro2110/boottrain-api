2e9b459c4f5eb2b8b35624bfa8d6b8745a2bf43e
Davi Castro <santos.davi2110@gmail.com>
2026-04-15 15:05:02 -0300

    chore: setup fastify api and add support to env files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, Boottrain!");
+import console from "console";
+import { fastify } from "fastify";
+
+const app = fastify({
+  logger: true,
+});
+
+app.get("/", async (request, reply) => {
+  return { message: "Hello, World!" };
+});
+
+try {
+  const port = Number(process.env.PORT) || 3000;
+
+  await app.listen({ port });
+
+  app.log.info(`Server listening at http://localhost:${port}`);
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
