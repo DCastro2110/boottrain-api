e42e1ff75cc3688a8da7c21f3bfbb717c7198655
Davi Castro <santos.davi2110@gmail.com>
2026-04-18 11:29:02 -0300

    refactor: add custom error classes for improved error handling

---
+++ src/errors/errors.ts
@@ -0,0 +1,44 @@
+export class NotFoundError extends Error {
+  code: string;
+  constructor(message: string) {
+    super(message);
+    this.name = "NotFoundError";
+    this.code = "NOT_FOUND";
+  }
+}
+
+export class UnauthorizedError extends Error {
+  code: string;
+  constructor(message: string) {
+    super(message);
+    this.name = "UnauthorizedError";
+    this.code = "UNAUTHORIZED";
+  }
+}
+
+export class ForbiddenError extends Error {
+  code: string;
+  constructor(message: string) {
+    super(message);
+    this.name = "ForbiddenError";
+    this.code = "FORBIDDEN";
+  }
+}
+
+export class BadRequestError extends Error {
+  code: string;
+  constructor(message: string) {
+    super(message);
+    this.name = "BadRequestError";
+    this.code = "BAD_REQUEST";
+  }
+}
+
+export class InternalServerError extends Error {
+  code: string;
+  constructor(message: string) {
+    super(message);
+    this.name = "InternalServerError";
+    this.code = "INTERNAL_SERVER_ERROR";
+  }
+}
