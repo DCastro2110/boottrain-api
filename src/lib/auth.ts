c8410717430e7722dad8790aa01f3a811530b154
Davi Castro <santos.davi2110@gmail.com>
2026-04-28 16:24:48 -0300

    chore: add Google OAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,8 +9,11 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  emailAndPassword: {
-    enabled: true,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
   },
   session: {
     additionalFields: {
